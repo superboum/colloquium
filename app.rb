9d545e50aabf885934b66282e2bcdc6350554214
Quentin Dufour <quentin@dufour.tk>
2014-12-16 10:59:18 +0100

    Dependencies structuration

--- app.rb
+++ app.rb
@@ -1,43 +1,12 @@
 require 'sinatra'
 require 'haml'
 
-get '/' do
-    haml :home
-end
+class ColloquiumApp < Sinatra::Base
+    get '/' do
+        haml :home
+    end
 
-get '/admin' do
-    haml :admin
+    get '/admin' do
+        haml :admin
+    end
 end
-
-#pages Jean
-  #page principale de Jean (page 1)
-get '/jean' do
-    haml :jean
-end
-
-get '/jean1' do
-    haml :jean
-end
-
-  #page 2
-get '/jean2' do
-    haml :jean2
-end
-
-  #page 3
-get '/jean3' do
-    haml :jean3
-end
-
-get '/thomas' do 
-    haml :thomas
-end
-
-get '/navigation' do
-    haml :navigation
-end
-
-get '/laurent' do
-	haml :laurent
-end
-
