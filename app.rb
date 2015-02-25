07e9bb9a2563cfb4bfedb57ef0811fcbfb7224b1
Quentin Dufour <quentin@dufour.tk>
2015-02-25 02:39:32 +0100

    Basic Article handling

--- app.rb
+++ app.rb
@@ -12,19 +12,48 @@ class ColloquiumApp < Sinatra::Base
         articles = Article.all
         haml :home, :locals => { :articles => articles }
     end
+    
+    get '/article/:id' do
+        article = Article.find_by_id(params[:id])
+        haml :article, :locals => { :article => article }
+    end
 
     get '/admin' do
-        haml :admin
+        articles = Article.all
+        haml :'admin/layout', :layout => :'layout' do
+          haml :'admin/home', :locals => { :articles => articles }
+        end
     end
 
-    post '/api/article' do
-        content_type :json
-        param = JSON.parse(request.body.read)
-        article = Article.new
-        article.title = param["title"]
-        article.category = param["category"]
-        article.short_text = param["short_text"]
-        article.long_text = param["long_text"]
-        article.save
+    get '/admin/article' do
+      articles = Article.all
+      haml :'admin/layout', :layout => :'layout' do
+        haml :'admin/article/home', :locals => { :articles => articles }
+      end
     end
+
+    get '/admin/article/new' do
+      haml :'admin/layout', :layout => :'layout' do
+        haml :'admin/article/new'
+      end
+    end
+
+    post '/admin/article/new' do
+        article = Article.new
+        article.title = params['title']
+        article.category = params['category']
+        article.short_text = params['short_text']
+        article.long_text = params['long_text']
+        article.save
+        redirect "/admin/article", 303
+    end
+
+    get '/admin/article/edit' do
+        haml :'admin/article/edit'
+    end
+
+    get '/admin/article/delete' do
+        haml :'admin/article/delete'
+    end
+
 end
