8ea0a663d87eb6825456e2714ca61a41eb907095
Quentin Dufour <quentin@dufour.tk>
2015-04-28 23:07:38 +0200

    First Settings implementation

--- routes/settings.rb
+++ routes/settings.rb
@@ -11,32 +11,35 @@ module SettingsController
 
     app.post '/admin/settings/?' do
       restrictToAdmin!
-	  store = YAML::Store.new "config/general.yml"
-	  store.transaction do
-      store["parameters"] = { 
-        "base_url" => params["base_url"],
-        "title"  => params["title"],
-        "short_title"  => params["short_title"],
-        "slogan" => params["slogan"],
-        "cover" => params["cover"],
-      }
-      store['payment'] = { 
-        'rib' => {
-          'code_banque' => params['code_banque'],
-          'code_guichet' => params['code_guichet'],
-          'numero_compte' => params['numero_compte'],
-          'cle_rib' => params['cle_rib'],
-          'domiciliation' => params['domiciliation'],
-          'iban' => params['iban'],
-          'bic' => params['bic']
-        },
-        'cheque' => {
-          'order' => params['order'],
-          'address' => params['address']
+      store = YAML::Store.new "config/general.yml"
+      store.transaction do
+        store["parameters"] = { 
+          "base_url" => params["base_url"],
+          "title"  => params["title"],
+          "short_title"  => params["short_title"],
+          "slogan" => params["slogan"],
+          "cover" => params["cover"],
         }
-      }
-	  end
+
+        store['payment'] = { 
+          'rib' => {
+            'code_banque' => params['code_banque'],
+            'code_guichet' => params['code_guichet'],
+            'numero_compte' => params['numero_compte'],
+            'cle_rib' => params['cle_rib'],
+            'domiciliation' => params['domiciliation'],
+            'iban' => params['iban'],
+            'bic' => params['bic']
+          },
+          'cheque' => {
+            'order' => params['order'],
+            'address' => params['address']
+          }
+        }
+      end
+
+      app.config_file 'config/general.yml'
       redirect "/admin/settings", 303
-	end
+    end
   end
 end
